66a1275fa79bb0ba53d6a864199800169fcd8525
Sylwester <Sylwester191@gmail.com>
2023-01-28 23:35:00 +0100

    preventDefault() added to input

--- js/js.js
+++ js/js.js
@@ -1,130 +1,76 @@
-
-const startBtn = document.querySelector('.start')
-const welcomePage = document.querySelector('.welcome-page')
-const winnerPage = document.querySelector('.winner-page')
-const main = document.querySelector('.main')
-const back = document.querySelector('.back')
+const startBtn = document.querySelector('.start');
+const welcomePage = document.querySelector('.welcome-page');
+const winnerPage = document.querySelector('.winner-page');
+const main = document.querySelector('.main');
+const back = document.querySelector('.back');
 const itemvalue = document.querySelector('.form-control');
-const elements = document.querySelector('.elements-container')
-const addBtn = document.querySelector('.add-item')
+const elements = document.querySelector('.elements-container');
+const addBtn = document.querySelector('.add-item');
 const error = document.querySelector(".error");
 const clear = document.querySelector('.clear');
-const delBtns = document.getElementsByClassName('deleteItem');
 const closeBox = document.querySelector('.close-box');
-const choose = document.querySelector('.chose')
-const result = document.querySelector('.tutaj')
-const botom = document.querySelector('.botom')
-const winner = document.querySelector('.winner-item')
-const eClick = document.getElementById('item')
-const repeat = document.querySelector('.repeat')
-
+const choose = document.querySelector('.chose');
+const winner = document.querySelector('.winner-item');
+const repeat = document.querySelector('.repeat');
 
 const closeFirst = () => {
-    welcomePage.classList.toggle('d-none')
-    main.classList.toggle('d-none')
-
-
+    welcomePage.classList.toggle('d-none');
+    main.classList.toggle('d-none');
 }
 
-
-
-
-
 const addNote = () => {
-    if (itemvalue.value != "") {
-
-        error.style.visibility = 'hidden'
-        createNote()
-
+    if (itemvalue.value !== "") {
+        error.style.visibility = 'hidden';
+        createNote();
     } else {
-        error.innerHTML = 'Musisz wpisac wartość!'
-        error.style.visibility = 'visible'
-
+        error.innerHTML = 'Musisz wpisac wartość!';
+        error.style.visibility = 'visible';
     }
-
 }
 
 let selectedValue;
 let cardID = 0;
-
-
-const array = []
+const array = [];
 
 const createNote = () => {
     const newNote = document.createElement('div');
-    newNote.classList.add('new-item')
-    newNote.setAttribute('id', cardID)
-
-    newNote.innerHTML = `
-
-    <p>${itemvalue.value}</p>
-`
-
-
-    array.push(itemvalue.value)
-
-
-    elements.appendChild(newNote)
-    cardID++
-    elements.value = '';
-    itemvalue.value = ''
-
+    newNote.classList.add('new-item');
+    newNote.setAttribute('id', cardID);
+    newNote.innerHTML = `<p>${itemvalue.value}</p>`;
+    array.push(itemvalue.value);
+    elements.appendChild(newNote);
+    cardID++;
+    itemvalue.value = '';
 }
 
 const los = () => {
     const losIt = array[Math.floor(Math.random() * array.length)];
-
-
-    if (array.length == 0 || array.length == 1) {
-        error.style.visibility = 'visible'
-        error.innerHTML = 'Musisz dodać minimum dwóch uczestników!!'
+    if (array.length < 2) {
+        error.style.visibility = 'visible';
+        error.innerHTML = 'Musisz dodać minimum dwóch uczestników!!';
     } else {
-        setTimeout(
-            function () {
-                winnerPage.classList.toggle('d-none')
-                main.classList.toggle('d-none')
-
-                winner.textContent = `
-                  ${losIt}`
-            }, 1000)
+        setTimeout(() => {
+            winnerPage.classList.toggle('d-none');
+            main.classList.toggle('d-none');
+            winner.textContent = `${losIt}`;
+        }, 1000);
     }
-
-
 }
-
-
-
-
-
-
 
 const deleteAll = () => {
-    elements.textContent = ''
+    elements.innerHTML = '';
     cardID = 0;
-    error.style.visibility = 'hidden'
-    itemvalue.value = ''
-    array.length = 0
+    error.style.visibility = 'hidden';
+    itemvalue.value = '';
+    array.length = 0;
 }
-
-
-
 
 const repeatLos = () => {
-    winnerPage.classList.toggle('d-none')
-    main.classList.toggle('d-none')
-    setTimeout(
-        los()), 2000
+    winnerPage.classList.toggle('d-none');
+    main.classList.toggle('d-none');
+    setTimeout(los, 1000);
 }
 
-
-
-eClick.onkeyup = function (e) {
-    if (e.key == 'Enter' && itemvalue.value >= '1') { createNote() }
-
-    else {
-        error.style.visibility = 'visible';
-    }
-}
 
 closeBox.addEventListener('click', function () {
     array.length = ""
@@ -132,6 +78,16 @@ closeBox.addEventListener('click', function () {
     elements.textContent = ''
     main.classList.toggle('d-none')
 })
+
+document.getElementById("item").addEventListener("keydown", function (event) {
+    // event.preventDefault();
+    if (event.key == 'Enter' && itemvalue.value >= '1') { event.preventDefault(); createNote() }
+
+    else {
+        error.style.visibility = 'visible';
+    }
+
+});
 
 repeat.addEventListener('click', repeatLos)
 clear.addEventListener('click', deleteAll)
